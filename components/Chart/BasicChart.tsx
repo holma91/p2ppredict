637f4788fc747bead904933d7d85871f13bdf37b
Alexander <alexanderholmberg91@gmail.com>
2022-07-01 16:33:53 +0200

    start removing dependence on pancake ui

--- components/Chart/BasicChart.tsx
+++ components/Chart/BasicChart.tsx
@@ -1,12 +1,5 @@
-import {
-  Box,
-  ButtonMenu,
-  ButtonMenuItem,
-  Flex,
-  Text,
-} from '@pancakeswap/uikit';
 import { useState, memo } from 'react';
-import PairPriceDisplay from './utils/PairPriceDisplay';
+import styled from 'styled-components';
 import { getTimeWindowChange } from './utils/utils';
 import { prices } from '../../data/mockPrices';
 import SwapLineChart from './SwapLineChart';
@@ -49,43 +42,25 @@ const BasicChart = ({
 
   return (
     <>
-      <Flex
-        flexDirection={['column', null, null, null, null, null, 'row']}
-        alignItems={['flex-start', null, null, null, null, null, 'center']}
-        justifyContent="space-between"
-        px="24px"
-      >
-        <Flex flexDirection="column" pt="12px">
-          <PairPriceDisplay
-            value={valueToDisplay}
-            inputSymbol={inputCurrency?.symbol}
-            outputSymbol={outputCurrency?.symbol}
-          >
-            <Text
-              color={isChangePositive ? 'success' : 'failure'}
-              fontSize="20px"
-              ml="4px"
-              bold
-            >
-              {`${isChangePositive ? '+' : ''}${changeValue.toFixed(
-                3
-              )} (${changePercentage}%)`}
-            </Text>
-          </PairPriceDisplay>
-          <Text small color="secondary">
-            {hoverDate || currentDate}
-          </Text>
-        </Flex>
-        <Box>
-          <ButtonMenu activeIndex={timeWindow} scale="sm">
-            <ButtonMenuItem>24H</ButtonMenuItem>
-            <ButtonMenuItem>1W</ButtonMenuItem>
-            <ButtonMenuItem>1M</ButtonMenuItem>
-            <ButtonMenuItem>1Y</ButtonMenuItem>
-          </ButtonMenu>
-        </Box>
-      </Flex>
-      <Box height={chartHeight} p={'16px'} width="100%">
+      <StyledFlex>
+        <div className="inner">
+          <div className="inner-inner">
+            <span className="price">72.01</span>
+            <span className="change">+0.940 (1.32%)</span>
+          </div>
+
+          <div className="date">{hoverDate || currentDate}</div>
+        </div>
+        <div>
+          <ButtonMenu2>
+            <Button active={timeWindow === 0}>24H</Button>
+            <Button active={false}>1W</Button>
+            <Button active={false}>1M</Button>
+            <Button active={false}>1Y</Button>
+          </ButtonMenu2>
+        </div>
+      </StyledFlex>
+      <Box height={chartHeight}>
         <SwapLineChart
           data={pairPrices}
           setHoverValue={setHoverValue}
@@ -97,6 +72,79 @@ const BasicChart = ({
     </>
   );
 };
+
+type BoxProps = {
+  height: string;
+};
+
+const Box = styled.div<BoxProps>`
+  height: ${({ height }) => height};
+  width: 100%;
+  padding: 1rem;
+`;
+
+const StyledFlex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1.5rem;
+  width: 100%;
+
+  .inner {
+    display: flex;
+    flex-direction: column;
+    padding-top: 0.75rem;
+  }
+
+  .date {
+    font-size: 0.9rem;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.primary};
+  }
+
+  .inner-inner {
+    display: flex;
+    align-items: end;
+    gap: 0.5rem;
+
+    .price {
+      font-size: 2.5rem;
+      font-weight: 600;
+    }
+
+    .change {
+      font-size: 1.25rem;
+      font-weight: 600;
+      padding-bottom: 0.25rem;
+    }
+  }
+`;
+
+const ButtonMenu2 = styled.div`
+  width: 100%;
+  display: flex;
+  background-color: #47b5ff;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+`;
+
+type ButtonProps = {
+  active: boolean;
+};
+
+const Button = styled.button<ButtonProps>`
+  padding: 0.5rem 0.75rem;
+  font-weight: 600;
+  font-size: 1rem;
+  border: 0;
+  background-color: ${({ theme, active }) =>
+    active ? theme.colors.primary : 'inherit'};
+  color: ${({ theme, active }) => (active ? 'white' : 'inherit')};
+
+  :hover {
+    cursor: pointer;
+    color: ${({ theme }) => theme.colors.primaryHover};
+  }
+`;
 
 export default memo(BasicChart, (prev, next) => {
   return (
