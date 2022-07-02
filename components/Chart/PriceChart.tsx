e7dd75f8f564156222b6ed0de2b7c1d7c4865ee4
Alexander <alexanderholmberg91@gmail.com>
2022-07-02 09:22:56 +0200

    replace pancake ui

--- components/Chart/PriceChart.tsx
+++ components/Chart/PriceChart.tsx
@@ -1,15 +1,6 @@
-import {
-  Button,
-  ExpandIcon,
-  Flex,
-  IconButton,
-  ShrinkIcon,
-  SyncAltIcon,
-  Text,
-  LineGraphIcon,
-} from '@pancakeswap/uikit';
 import styled from 'styled-components';
 import BasicChart from './BasicChart';
+
 const StyledPriceChart = styled.div<{
   $isDark: boolean;
   $isExpanded: boolean;
@@ -17,25 +8,14 @@ const StyledPriceChart = styled.div<{
 }>`
   border: none;
   border-radius: 0.2rem;
-  width: 100%;
   padding-top: 36px;
 
   padding-top: 8px;
   background: ${({ $isDark }) =>
     $isDark ? 'rgba(39, 38, 44, 0.5)' : 'rgba(255, 255, 255, 0.5)'};
   border: ${({ theme }) => `1px solid ${theme.colors.cardBorder}`};
-  width: ${({ $isExpanded, $isFullWidthContainer }) =>
-    $isFullWidthContainer || $isExpanded ? '100%' : '50%'};
-  height: ${({ $isExpanded }) =>
-    $isExpanded ? 'calc(100vh - 100px)' : '516px'};
-`;
-// ${({ theme }) => theme.mediaQueries.sm} {
-
-const ChartButton = styled(Button)`
-  background-color: ${({ $active, theme }) =>
-    $active && `${theme.colors.primary}0f`};
-  padding: 4px 8px;
-  border-radius: 6px;
+  width: '70%';
+  height: '516px';
 `;
 
 type PriceChartProps = {
@@ -48,6 +28,22 @@ type PriceChartProps = {
   currentSwapPrice: any;
 };
 
+const StyledFlex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 1.5rem;
+
+  .styledFlex-inner {
+    display: flex;
+    align-items: center;
+
+    .chosen-assets {
+      font-size: ${({ theme }) => theme.typeScale.paragraph};
+      font-weight: 600;
+    }
+  }
+`;
+
 const PriceChart = ({
   inputCurrency,
   outputCurrency,
@@ -57,46 +53,17 @@ const PriceChart = ({
   token1Address,
   currentSwapPrice,
 }: PriceChartProps) => {
-  const isDesktop = true;
-
   return (
     <StyledPriceChart
       $isDark={isDark}
       $isExpanded={false}
       $isFullWidthContainer={isFullWidthContainer}
     >
-      <Flex justifyContent="space-between" px="24px">
-        <Flex alignItems="center">
-          {inputCurrency && (
-            <Text color="text" bold>
-              {outputCurrency
-                ? `${inputCurrency.symbol}/${outputCurrency.symbol}`
-                : inputCurrency.symbol}
-            </Text>
-          )}
-          <IconButton variant="text">
-            <SyncAltIcon ml="6px" color="primary" />
-          </IconButton>
-          <Flex>
-            <ChartButton
-              aria-label="Basic"
-              title="Basic"
-              $active={true}
-              scale="sm"
-              variant="text"
-              color="primary"
-              mr="8px"
-            >
-              {isDesktop ? 'Basic' : <LineGraphIcon color="primary" />}
-            </ChartButton>
-          </Flex>
-        </Flex>
-        <Flex>
-          <IconButton variant="text">
-            <ExpandIcon color="text" />
-          </IconButton>
-        </Flex>
-      </Flex>
+      <StyledFlex>
+        <div className="styledFlex-inner">
+          <div className="chosen-assets">BNB/CAKE</div>
+        </div>
+      </StyledFlex>
       <BasicChart
         token0Address={token0Address}
         token1Address={token1Address}
