b1519c64b1b07bcc939e6f2e45060ee39d454e85
Alexander <alexanderholmberg91@gmail.com>
2022-08-21 14:07:31 +0200

    black and white logos

--- frontend/components/Banner.tsx
+++ frontend/components/Banner.tsx
@@ -1,6 +1,56 @@
 import { useState, Dispatch, SetStateAction } from 'react';
 import styled from 'styled-components';
+import { useNetwork } from 'wagmi';
 import { assetToImage } from '../utils/misc';
+
+const isBlackAndWhite = (activeChain: string | undefined, symbol: string) => {
+	let colored: string[] = [];
+	if (activeChain === 'rinkeby') {
+		colored = ['eth', 'btc', 'matic', 'usdc', 'bnb', 'atom', 'link'];
+	} else if (activeChain === 'maticmum') {
+		colored = ['eth', 'btc', 'matic', 'usdc', 'usdt', 'dai'];
+	} else {
+		colored = [
+			'aave',
+			'ada',
+			'algo',
+			'ape',
+			'avax',
+			'axs',
+			'bnb',
+			'btc',
+			'eth',
+			'busd',
+			'crv',
+			'dai',
+			'doge',
+			'dot',
+			'ftm',
+			'icp',
+			'link',
+			'ltc',
+			'matic',
+			'mkr',
+			'shib',
+			'snx',
+			'sol',
+			'sushi',
+			'trx',
+			'uni',
+			'usdc',
+			'usdt',
+			'vet',
+			'xmr',
+			'xtz',
+			'yfi',
+			'zec',
+		];
+	}
+	if (colored.includes(symbol)) {
+		return false;
+	}
+	return true;
+};
 
 export default function Banner({
 	bannerChoice,
@@ -15,21 +65,25 @@ export default function Banner({
 	setBannerChoice: Dispatch<SetStateAction<string>>;
 	setActive: Dispatch<SetStateAction<number>>;
 }) {
+	const { chain } = useNetwork();
+
 	const handleBannerChange = (symbol: string) => {
+		if (!chain || !chain.network) return;
+		if (isBlackAndWhite(chain.network, symbol)) return;
 		setActive(0);
 		setBannerChoice(symbol);
 	};
 
 	return (
 		<Container fullWidth={fullWidth}>
-			{showAll && (
-				<BannerDiv active={bannerChoice === 'all'}>
-					<p>ALL MARKETS</p>
-				</BannerDiv>
-			)}
 			{Object.keys(assetToImage).map(symbol => {
 				return (
-					<BannerDiv active={bannerChoice === symbol} key={symbol} onClick={() => handleBannerChange(symbol)}>
+					<BannerDiv
+						isBlackAndWhite={isBlackAndWhite(chain?.network, symbol)}
+						active={bannerChoice === symbol}
+						key={symbol}
+						onClick={() => handleBannerChange(symbol)}
+					>
 						<img src={assetToImage[symbol]} alt={`${symbol}-logo`} />
 					</BannerDiv>
 				);
@@ -38,23 +92,26 @@ export default function Banner({
 	);
 }
 
-const BannerDiv = styled.div<{ active: boolean }>`
+const BannerDiv = styled.div<{ isBlackAndWhite: boolean; active: boolean }>`
 	padding: 0.8rem;
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	border-bottom: 3px solid ${({ theme, active }) => (active ? theme.colors.secondary : theme.colors.gray[300])};
-
+	border-bottom: 3px solid ${({ theme, active }) => (active ? theme.colors.primary : theme.colors.gray[300])};
 	:hover {
-		cursor: pointer;
-		border-bottom: 3px solid ${({ theme }) => theme.colors.secondary};
+		cursor: ${({ isBlackAndWhite }) => (isBlackAndWhite ? '' : 'pointer')};
+		border-bottom: 3px solid
+			${({ isBlackAndWhite, theme }) => (isBlackAndWhite ? theme.colors.gray[300] : theme.colors.primary)};
 	}
-
 	img {
 		height: 28px;
 		width: 28px;
+		filter: ${({ isBlackAndWhite }) => (isBlackAndWhite ? 'gray' : '')}; /* IE6-9 */
+		-webkit-filter: ${({ isBlackAndWhite }) =>
+			isBlackAndWhite ? 'grayscale(1)' : ''}; /* Google Chrome, Safari 6+ & Opera 15+ */
+		filter: ${({ isBlackAndWhite }) =>
+			isBlackAndWhite ? 'grayscale(1)' : ''}; /* Google Chrome, Safari 6+ & Opera 15+ */
 	}
-
 	p {
 		display: flex;
 		align-items: center;
